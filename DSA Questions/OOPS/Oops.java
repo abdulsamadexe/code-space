d23d81cc81afd711faafd290be50109c26cc2b7c
Abdul Samad <abdulsamadbrohi461@gmail.com>
2024-06-07 03:17:46 +0500

    recursion two Questions

--- DSA Questions/OOPS/Oops.java
+++ DSA Questions/OOPS/Oops.java
@@ -3,22 +3,22 @@ public class Oops {
         Pen a=new Pen();
         a.setColor("blue");
         a.setTip(5);
-        System.out.println(a.getColor);
+        System.out.println(a.getColor());
     }
 }
-class Pen{
-    private int tip;
-    private String color;
-    public static void setColor(String color){
-        this.color=color;
+    class Pen{
+        private int tip;
+        private String color;
+        public void setColor(String color){
+            this.color=color;
+        }
+        public void setTip(int tip){
+            this.tip=tip;
+        }
+        public String getColor(){
+            return this.color;
+        }
+        public int getTip(){
+            return this.tip;
+        }
     }
-    public static void setTip(int tip){
-        this.tip=tip;
-    }
-    public static void getColor{
-        return this.color;
-    }
-    public static void getTip(){
-        return this.tip;
-    }
-}
---
+++ DSA Questions/recursion/recursion1.java
@@ -0,0 +1,16 @@
+package recursion;
+
+public class recursion1{
+    public static  void printDec(int n){
+        if(n==1){
+            System.out.print(n+ " ");
+            return;
+        }
+        System.out.print(n+ " ");
+        printDec(n-1);
+    }
+    public static void main(String args[]){
+        printDec(12);
+
+    }
+}
